68aa5688d1b4014fade480346c50584c6af8352d
Nicolas Patron <nicolas.patron@hotmail.fr>
2025-11-07 14:57:35 +0100

    wip

--- webapp/src/App.jsx
+++ webapp/src/App.jsx
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   App.jsx                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: fpalumbo <fpalumbo@student.42.fr>          +#+  +:+       +#+        */
+/*   By: npatron <npatron@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/22 17:43:01 by npatron           #+#    #+#             */
-/*   Updated: 2024/12/27 18:09:42 by fpalumbo         ###   ########.fr       */
+/*   Updated: 2025/11/06 17:07:13 by npatron          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,29 +19,12 @@ import { useLocation, useNavigate } from 'react-router-dom';
 
 
 import HomePage from './components/HomePage';
-import LoginRegister from './components/LoginOrRegister';
-import Register from './components/Register';
-import Login from './components/Login';
-import TetrisGame from './components/TetrisGame';
-import { UserAuthProvider } from './providers/UserAuthProvider';
 
 const App = () => {
 
 	const navigate = useNavigate();
-
-	const location = useLocation();
-	const publicPaths = ["/", "/register", "/check42user"];
-	const isPublicRoute = publicPaths.includes(location.pathname);
-
 	const goToHome = () => {
-
-		const myJwt = localStorage.getItem("jwt")
-
-		if (myJwt)
-			navigate("/home");
-		else
-			navigate("/");
-		return ;
+		navigate('/');
 	}
 
 	return (
@@ -49,17 +32,9 @@ const App = () => {
 				<div className="header">
 					<span className="header-write" onClick={() => goToHome()}>RED-TETRIS</span>
 				</div>
-			<Routes>
-					<Route path="/" element={<LoginRegister />} />
-			</Routes>
-			<UserAuthProvider>
 				<Routes>
-					<Route path="/home" element={<HomePage />} />
-					<Route path="/register" element={<Register />} />
-					<Route path="/login" element={<Login />} />
-					<Route path="/:roomId/:playerName" element={<TetrisGame />} />
+					<Route path="/" element={<HomePage />} />
 				</Routes>
-			</UserAuthProvider>
 			</div>
   );
 };
